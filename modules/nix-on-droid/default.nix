e57dee2990ad96a5ab1667688b90f7b4d121600e
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-08-25 16:14:04 +0200

    Add default nix-on-droid module

--- modules/nix-on-droid/default.nix
+++ modules/nix-on-droid/default.nix
@@ -1,6 +1,15 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 {
   flake.nixOnDroidModules = {
+    default = self.nixOnDroidModules.all;
+    all = {
+      imports = [
+        self.nixOnDroidModules.my
+        self.nixOnDroidModules.extra
+        self.nixOnDroidModules.nixosCompat
+        self.nixOnDroidModules.legacy
+      ];
+    };
     my = import ./my;
     extra = import ./extra;
     nixosCompat = import ./nixos-compat inputs;
