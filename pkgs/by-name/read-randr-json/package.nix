1f02dd2800694986e3b8901fbda686ab245cc74c
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-08-28 12:00:01 +0200

    Add read-randr-json package

---
+++ pkgs/by-name/read-randr-json/package.nix
@@ -0,0 +1,28 @@
+{ stdenv
+, fetchFromGitHub
+, xorg
+}:
+
+let
+  pname = "read-randr-json";
+  version = "main";
+  src = fetchFromGitHub {
+    owner = "zignageX";
+    repo = pname;
+    rev = "main";
+    hash = "sha256-IhL6SxmO0zg7a+xVOFH973+ibXYb0+M1F6WVAP8+sas=";
+  };
+in
+stdenv.mkDerivation {
+  inherit pname version src;
+
+  buildInputs = [
+    xorg.libxcb.dev
+  ];
+
+  makeFlags = [ "prefix=$(out)" ];
+
+  patches = [
+    ./checkout.patch
+  ];
+}
