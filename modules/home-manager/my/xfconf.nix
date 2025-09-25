c2ca198060538fc76514e333e2da9d5c731cca9e
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-09-25 10:56:10 +0200

    Disable xfconf settings for now

--- modules/home-manager/my/xfconf.nix
+++ modules/home-manager/my/xfconf.nix
@@ -5,20 +5,20 @@ let
 in
 {
   # TODO: Currently checks if it's a graphical environment by checking if vscode is enabled
-  xfconf.settings = mkIf (cfgOs.type != "nix-on-droid" && config.programs.vscode.enable) {
-    pointers = {
-      "PIXA385400_093A0274_Touchpad/ReverseScrolling" = true;
-    };
-    xsettings = {
-      "Net/ThemeName" = "Mint-Y-Dark-Aqua";
-      "Net/IconThemeName" = "Mint-Y-Aqua";
-      "Gtk/CursorThemeName" = "Bibata-Modern-Classic";
-    };
-    xfce4-keyboard-shortcuts = {
-      "commands/custom/<Super>r" = null;
-      "commands/custom/<Super>e" = null;
-      "commands/custom/<Alt>F1" = null;
-      "commands/custom/<Primary><Alt>t" = null;
-    };
-  };
+  # xfconf.settings = mkIf (cfgOs.type != "nix-on-droid" && config.programs.vscode.enable) {
+  #   pointers = {
+  #     "PIXA385400_093A0274_Touchpad/ReverseScrolling" = true;
+  #   };
+  #   xsettings = {
+  #     "Net/ThemeName" = "Mint-Y-Dark-Aqua";
+  #     "Net/IconThemeName" = "Mint-Y-Aqua";
+  #     "Gtk/CursorThemeName" = "Bibata-Modern-Classic";
+  #   };
+  #   xfce4-keyboard-shortcuts = {
+  #     "commands/custom/<Super>r" = null;
+  #     "commands/custom/<Super>e" = null;
+  #     "commands/custom/<Alt>F1" = null;
+  #     "commands/custom/<Primary><Alt>t" = null;
+  #   };
+  # };
 }
