30fed2ba66e133d6a8fdde2bb2eecef435909257
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-09-25 09:50:35 +0200

    Add "stable" cros kernel

---
+++ pkgs/by-name/linuxPackages_cros.nix
@@ -0,0 +1,12 @@
+{ ninelore-monoflake
+, ninelore-monoflake-pkgs
+, linux_cros
+}:
+
+let
+  upstreamPackages = ninelore-monoflake-pkgs.linuxPackagesFor linux_cros;
+  crossCompiledPackages = ninelore-monoflake-pkgs.linuxPackagesFor linux_cros.cross-compiled;
+in
+upstreamPackages // {
+  cross-compiled = crossCompiledPackages;
+}
---
+++ pkgs/by-name/linux_cros.nix
@@ -0,0 +1,16 @@
+{ ninelore-monoflake, ninelore-monoflake-input, system, ... } @ args:
+let
+  upstreamKernel = ninelore-monoflake.linux_cros.override args;
+  crossCompiledKernel =
+    let
+      pkgsCross = import ninelore-monoflake-input.inputs.nixpkgs {
+        localSystem = "x86_64-linux";
+        crossSystem = system;
+      };
+      crossPkg = pkgsCross.callPackage (ninelore-monoflake-input + "/pkgs/linux_cros") args;
+    in
+    crossPkg;
+in
+upstreamKernel // {
+  cross-compiled = crossCompiledKernel;
+}
