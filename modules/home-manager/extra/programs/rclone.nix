4f111ee1b4269ca0dc36c87f15e1c0476a731d31
Waldemar Tomme <waldetomme@gmail.com>
2025-09-28 00:07:16 +0200

    Resync if local path does not exist yet

--- modules/home-manager/extra/programs/rclone.nix
+++ modules/home-manager/extra/programs/rclone.nix
@@ -1,8 +1,30 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 with lib;
 let
   cfg = config.programs.rclone;
   replaceSlashes = builtins.replaceStrings [ "/" " " ] [ "-" "-" ];
+  bisyncScript =
+    let
+      rcloneCmd = "rclone bisync \"$_remotePath\" \"$_localPath\" \"$@\" --resilient --recover";
+    in
+    pkgs.writeShellApplication {
+      name = "rclone-bisync";
+      runtimeInputs = [
+        cfg.package
+      ];
+      text = ''
+        _remotePath="$1"
+        _localPath="$2"
+        shift
+        shift
+        if [ ! -e "$_localPath" ]; then
+          mkdir -p "$_localPath"
+          ${rcloneCmd} --resync
+        else
+          ${rcloneCmd}
+        fi
+      '';
+    };
 in
 {
   options.programs.rclone = {
@@ -23,13 +45,13 @@ in
                   };
                   flags = mkOption {
                     type = types.listOf types.str;
-                    default = [];
+                    default = [ ];
                     apply = escapeShellArgs;
                   };
                 };
               }
             ));
-            default = {};
+            default = { };
           };
         };
       });
@@ -60,11 +82,9 @@ in
             Unit.Description = "Regular rclone bisync for ${remoteName}:${remotePath}";
             Service = {
               ExecStart = concatStringsSep " " [
-                (getExe cfg.package)
-                "bisync"
+                (getExe bisyncScript)
                 (escapeShellArg "${remoteName}:${remotePath}")
                 (escapeShellArg bisyncConfig.localPath)
-                "--dry-run"
                 bisyncConfig.flags
               ];
             };
@@ -81,34 +101,34 @@ in
           };
         units =
           foldl
-          recursiveUpdate
-          {}
-          (
-            flatten
+            recursiveUpdate
+            { }
             (
-              map
-              (
-                { name, value }:
-                let
-                  remoteName = name;
-                in
-                map
+              flatten
                 (
-                  { name, value }:
-                  let
-                    remotePath = name;
-                  in
-                    mkBisyncUnits {
-                      inherit remoteName remotePath;
-                      bisyncConfig = value;
-                    }
+                  map
+                    (
+                      { name, value }:
+                      let
+                        remoteName = name;
+                      in
+                      map
+                        (
+                          { name, value }:
+                          let
+                            remotePath = name;
+                          in
+                          mkBisyncUnits {
+                            inherit remoteName remotePath;
+                            bisyncConfig = value;
+                          }
+                        )
+                        (attrsToList (filterAttrs (n: v: v.enable) value.bisync))
+                    )
+                    (attrsToList cfg.remotes)
                 )
-                (attrsToList (filterAttrs (n: v: v.enable) value.bisync))
-              )
-              (attrsToList cfg.remotes)
-            )
-          );
+            );
       in
-        units;
+      units;
   };
 }
