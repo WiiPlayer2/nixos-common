16ab9701106b61249d5270c110d112e784cce0cb
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-10-06 08:55:25 +0200

    Don't install .NET 10 yet

--- modules/legacy/20-implementations/home/programs/dotnet-sdk.nix
+++ modules/legacy/20-implementations/home/programs/dotnet-sdk.nix
@@ -14,7 +14,8 @@ in
         with dotnetCorePackages;
         combinePackages [
           sdk_8_0
-          sdk_10_0
+          sdk_9_0
+          # sdk_10_0 # still rc
         ]
       )
     ];
