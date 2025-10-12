500397acdda18e83854d32463d058f3c33cb26a5
Waldemar Tomme <waldetomme@gmail.com>
2025-10-12 09:55:28 +0200

    Actually output private key

--- modules/_shared/age.nix
+++ modules/_shared/age.nix
@@ -23,5 +23,6 @@
       publicKeyFile=${lib.escapeShellArg (lib.removeSuffix ".age" file + ".pub")}
       privateKey=$(exec 3>&1; ${pkgs.openssh}/bin/ssh-keygen -q -t ed25519 -N "" -C ${lib.escapeShellArg "${target}:${name}"} -f /proc/self/fd/3 <<<y >/dev/null 2>&1; true)
       echo "$privateKey" | ssh-keygen -f /proc/self/fd/0 -y > "$publicKeyFile"
+      echo "$privateKey"
     '';
 }
