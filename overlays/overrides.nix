ee9a19e556fd4d254f2a48dd2dc3b512cf77383b
Waldemar Tomme <waldetomme@gmail.com>
2025-11-03 13:08:58 +0100

    Remove pysilero-vad override

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -3,21 +3,22 @@
     final: prev: {
       python312 = prev.python312.override {
         packageOverrides = pfinal: pprev: {
-          pysilero-vad = pprev.pysilero-vad.overrideAttrs (prevPkg: {
-            # version = "2.1.1";
-            # src = prev.fetchFromGitHub {
-            #   owner = "rhasspy";
-            #   repo = "pysilero-vad";
-            #   tag = "v2.1.1";
-            #   hash = "sha256-zxvYvPnL99yIVHrzbRbKmTazzlefOS+s2TAWLweRSYE=";
-            # };
-            # doCheck = false;
-            dontUsePytestCheck = true;
-            pythonImportsCheck = [ ];
-            meta = prevPkg.meta // {
-              broken = false;
-            };
-          });
+          # Used for wyoming-satellite; should probably be fixed better
+          # pysilero-vad = pprev.pysilero-vad.overrideAttrs (prevPkg: {
+          #   # version = "2.1.1";
+          #   # src = prev.fetchFromGitHub {
+          #   #   owner = "rhasspy";
+          #   #   repo = "pysilero-vad";
+          #   #   tag = "v2.1.1";
+          #   #   hash = "sha256-zxvYvPnL99yIVHrzbRbKmTazzlefOS+s2TAWLweRSYE=";
+          #   # };
+          #   # doCheck = false;
+          #   dontUsePytestCheck = true;
+          #   pythonImportsCheck = [ ];
+          #   meta = prevPkg.meta // {
+          #     broken = false;
+          #   };
+          # });
         };
       };
     };
