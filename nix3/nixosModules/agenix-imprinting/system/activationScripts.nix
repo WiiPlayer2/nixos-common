7ec30cc4b6a8d9e535ecf7f602e5043b13d309a8
Waldemar Tomme <waldetomme@gmail.com>
2025-11-23 19:49:28 +0100

    Use packaged imprinting files

--- nix3/nixosModules/agenix-imprinting/system/activationScripts.nix
+++ nix3/nixosModules/agenix-imprinting/system/activationScripts.nix
@@ -4,13 +4,15 @@ let
   cfg = config.age.imprinting;
   imprintingIdentityArg =
     if cfg.imprintingIdentityFile != null
-    then "-i ${escapeShellArg cfg.imprintingIdentityFile}"
+    then "-i ${escapeShellArg "${super.imprintingPkg}/imprinting-identity"}"
     else "";
   pathEnv =
     if cfg.plugins != [ ]
     then "PATH=\"${makeBinPath cfg.plugins}:$PATH\""
     else "";
   targetArg = escapeShellArg cfg.target;
+  imprintingFileArg =
+    escapeShellArg "${super.imprintingPkg}/imprinting-file";
 in
 mkIf cfg.enable {
   agenixNewGeneration.deps = [ "agenixImprinting" ];
@@ -21,7 +23,7 @@ mkIf cfg.enable {
       if [ ! -f ${targetArg} ]; then
         echo "Imprinting ${targetArg}..."
 
-        if ! ${pathEnv} ${getExe pkgs.age} -d ${imprintingIdentityArg} -o ${targetArg} ${escapeShellArg cfg.imprintingFile}; then
+        if ! ${pathEnv} ${getExe pkgs.age} -d ${imprintingIdentityArg} -o ${targetArg} ${imprintingFileArg}; then
           echo "Imprinting failed. To retry ensure the target file does not exist when activating again."
           sleep 10
           _localstatus=1
