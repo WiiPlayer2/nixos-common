ff44b07a1e2a863fe7d084cd41129f42f1e6ec40
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-11-25 14:59:42 +0100

    Add more modules to search content

--- pkgs/by-name/modules-search-content.nix
+++ pkgs/by-name/modules-search-content.nix
@@ -1,7 +1,8 @@
 { lib
 , system
 , inputs
-,
+
+, formats
 }:
 with lib;
 
@@ -10,12 +11,16 @@ with lib;
 let
   # TODO: make better somehow
   pkgs' = {
+    inherit formats;
+
     age-plugin-amnesia = { };
     ledmatrix-widgets = { };
     libvirt-autoballoon = { };
     snapcast = { };
     waydroid = { };
     weston = { };
+    davmail = { };
+    nix-web = { };
   };
 in
 inputs.nueschtos.packages.${system}.mkMultiSearch {
@@ -27,7 +32,7 @@ inputs.nueschtos.packages.${system}.mkMultiSearch {
     }
 
     {
-      name = "nixos-common";
+      name = "nixos-common (NixOS)";
       urlPrefix = "https://github.com/WiiPlayer2/nixos-common/tree/main/";
       specialArgs = {
         pkgs = pkgs';
@@ -41,5 +46,58 @@ inputs.nueschtos.packages.${system}.mkMultiSearch {
         inputs.self.nixosModules.wireplumber-roles
       ];
     }
+
+    {
+      name = "nixos-common (Home-Manager)";
+      urlPrefix = "https://github.com/WiiPlayer2/nixos-common/tree/main/";
+      specialArgs = {
+        pkgs = pkgs';
+      };
+      modules = [
+        inputs.self.homeModules.extra
+        inputs.self.homeModules.legacy
+        inputs.self.homeModules.legacy-standalone
+        inputs.self.homeModules.my
+        inputs.self.homeModules.profile-gaming
+        inputs.self.homeModules.profile-workstation
+        inputs.self.homeModules.program-archipelago
+        inputs.self.homeModules.program-bizhawk
+        inputs.self.homeModules.program-dolphin-emu
+        inputs.self.homeModules.program-poptracker
+      ];
+    }
+
+    # TODO: nix-on-droid modules needed
+    {
+      name = "nixos-common (nix-on-droid)";
+      urlPrefix = "https://github.com/WiiPlayer2/nixos-common/tree/main/";
+      specialArgs = {
+        pkgs = pkgs';
+      };
+      modules = [
+        # inputs.self.nixOnDroidModules.extra 
+        # inputs.self.nixOnDroidModules.legacy
+        inputs.self.nixOnDroidModules.my
+        # inputs.self.nixOnDroidModules.nixosCompat
+        inputs.self.nixOnDroidModules.profile-base
+      ];
+    }
+
+    # {
+    #   name = "Inputs (NixOS)";
+    #   modules = [
+    #     inputs.lanzaboote.nixosModules.lanzaboote
+    #     inputs.sops-nix.nixosModules.sops
+    #     inputs.NixVirt.nixosModules.default
+    #     inputs.stylix.nixosModules.stylix
+    #     inputs.nixpkgs-xr.nixosModules.nixpkgs-xr
+    #     inputs.agenix.nixosModules.default
+    #     inputs.agenix-rekey.nixosModules.default
+    #     inputs.disko.nixosModules.default
+    #     inputs.home-manager.nixosModules.home-manager
+    #     inputs.comin.nixosModules.comin
+    #     inputs.nixos-generators.nixosModules.all-formats
+    #   ];
+    # }
   ];
 }
