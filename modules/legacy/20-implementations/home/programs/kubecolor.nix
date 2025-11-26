562504ef61804e4c4549537cb3faa9c0debb32ec
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-11-26 15:30:11 +0100

    Explicitly add kubetcl when enabling kubecolor

--- modules/legacy/20-implementations/home/programs/kubecolor.nix
+++ modules/legacy/20-implementations/home/programs/kubecolor.nix
@@ -1,10 +1,14 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 with lib;
 let
   cfg = config.my.programs.kubecolor;
 in
 {
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      kubectl
+    ];
+
     programs.kubecolor = {
       enable = true;
       enableAlias = true;
