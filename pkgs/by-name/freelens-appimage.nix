9887b7962c0d9c42588c63fdc30d8c046aa4488d
CI <ci@home>
2025-12-04 04:16:19 +0000

    freelens-appimage: 1.6.2 -> 1.7.0
    Diff: https://github.com/freelensapp/freelens/compare/v1.6.2...v1.7.0

--- pkgs/by-name/freelens-appimage.nix
+++ pkgs/by-name/freelens-appimage.nix
@@ -6,7 +6,7 @@
 }:
 let
   pname = "freelens-appimage";
-  version = "1.6.2";
+  version = "1.7.0";
   icon = fetchurl {
     url = "https://raw.githubusercontent.com/freelensapp/freelens/v${version}/freelens/build/icons/512x512.png";
     hash = "sha256-YgugB0dJrr/fFwA9H8E0Oc3hhyvFIolR+RetXR01Y7Y=";
@@ -24,7 +24,7 @@ appimageTools.wrapType2 {
 
   src = fetchurl {
     url = "https://github.com/freelensapp/freelens/releases/download/v${version}/Freelens-${version}-linux-amd64.AppImage";
-    hash = "sha256-92hiXEwe6w2rm7QUN/KcgW4Ro9OydOwNawaW3cZiJGA=";
+    hash = "sha256-VeWTfJf66Cq4ZyR/mO0kzm8wD+Auo1MZvXPYC1Bbf7U=";
   };
 
   extraInstallCommands = ''
