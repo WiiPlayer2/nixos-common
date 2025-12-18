863bd3ee4393ffdde914970a91f3296ad60887cd
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-12-18 09:40:47 +0100

    Add cinnamon screensaver

---
+++ nix4/homeModules/profile-workstation/tree/i3/screensaver.nix
@@ -0,0 +1,21 @@
+{ lib, pkgs, ... }:
+with lib;
+{
+  home.packages = with pkgs; [
+    cinnamon-screensaver
+  ];
+
+  xsession.windowManager.i3.config = {
+    startup = [
+      { command = "${pkgs.cinnamon-screensaver}/bin/cinnamon-screensaver --hold"; }
+    ];
+
+    keybindings =
+      # let
+      #   modifier = config.xsession.windowManager.i3.config.modifier;
+      # in
+      mkOptionDefault {
+        "Control+Mod1+L" = "exec ${pkgs.cinnamon-screensaver}/bin/cinnamon-screensaver-command --lock";
+      };
+  };
+}
