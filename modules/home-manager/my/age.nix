6007b44308e9548cd8db59dbfca4e038b0216e08
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-12-18 12:24:20 +0100

    Disable yubikey identites in common

--- modules/home-manager/my/age.nix
+++ modules/home-manager/my/age.nix
@@ -16,12 +16,12 @@ in
       mkDefault "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhujcXdQyd72976pL0nrspx1xn6JprfgrFuULMzI64p"
     );
 
-    masterIdentities = [
-      {
-        identity = flakeRoot + "/secrets/identities/yubikey.pub";
-        pubkey = "age1yubikey1qfphf9vkgzv3kt80nuz5dpeuchmxa6tcsaypkmzvl6ngtnd34jmysqm4vaz";
-      }
-    ];
+    # masterIdentities = [
+    #   {
+    #     identity = flakeRoot + "/secrets/identities/yubikey.pub";
+    #     pubkey = "age1yubikey1qfphf9vkgzv3kt80nuz5dpeuchmxa6tcsaypkmzvl6ngtnd34jmysqm4vaz";
+    #   }
+    # ];
     extraEncryptionPubkeys = [
       "age1pvdtjne22xgkra7wh9nz6m7dtcjmfn3qx5tyu3gzpqmaey4e2s4qj4pa3s"
     ];
--- modules/legacy/20-implementations/nixos/config.nix
+++ modules/legacy/20-implementations/nixos/config.nix
@@ -69,12 +69,12 @@ in
 
   age.rekey = {
     hostPubkey = mkIf (cfg.hostPubkey != null) cfg.hostPubkey;
-    masterIdentities = [
-      {
-        identity = flakeRoot + "/secrets/identities/yubikey.pub";
-        pubkey = "age1yubikey1qfphf9vkgzv3kt80nuz5dpeuchmxa6tcsaypkmzvl6ngtnd34jmysqm4vaz";
-      }
-    ];
+    # masterIdentities = [
+    #   {
+    #     identity = flakeRoot + "/secrets/identities/yubikey.pub";
+    #     pubkey = "age1yubikey1qfphf9vkgzv3kt80nuz5dpeuchmxa6tcsaypkmzvl6ngtnd34jmysqm4vaz";
+    #   }
+    # ];
     extraEncryptionPubkeys = [
       "age1pvdtjne22xgkra7wh9nz6m7dtcjmfn3qx5tyu3gzpqmaey4e2s4qj4pa3s"
     ];
