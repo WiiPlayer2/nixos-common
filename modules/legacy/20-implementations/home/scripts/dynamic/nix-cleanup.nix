46d7dd1be2862612462877db168c3f74f8843da0
Waldemar Tomme <waldetomme@gmail.com>
2025-12-20 17:05:41 +0100

    Delete auto roots when cleaning up

--- modules/legacy/20-implementations/home/scripts/dynamic/nix-cleanup.nix
+++ modules/legacy/20-implementations/home/scripts/dynamic/nix-cleanup.nix
@@ -5,13 +5,20 @@
 let
   mainUser = config.my.config.mainUser.name;
   isRootMainUser = mainUser == "root";
-  collectCmd = "nix-collect-garbage --delete-older-than 3d";
+  collectCmd = "nix-collect-garbage --delete-old";
 in
 writeShellApplication {
   name = "nix-cleanup";
   text = ''
     # Always run as root
     [ "$(id -u)" != 0 ] && exec sudo "$0"
+
+    echo "=== [ Remove auto roots ] ==="
+    for root in /nix/var/nix/gcroots/auto/*; do
+      if [[ -e "$root" ]]; then
+        rm -v "$(readlink "$root")"
+      fi
+    done
 
     echo "=== [ Collect system garbage ] ==="
     ${collectCmd}
