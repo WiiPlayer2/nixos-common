5a3b3e050ba920ff49e780650bc4e39bb283e561
Waldemar Tomme <waldetomme@gmail.com>
2025-12-23 23:07:03 +0100

    Configure yubikey and fingerprint auth for all PAM services by default

--- modules/legacy/20-implementations/nixos/features/fingerprint.nix
+++ modules/legacy/20-implementations/nixos/features/fingerprint.nix
@@ -4,14 +4,16 @@ let
   cfg = config.my.features.fingerprint;
 in
 {
+  options.security.pam.services = mkOption {
+    type = types.attrsOf (
+      types.submodule {
+        config.fprintAuth = mkDefault true;
+      }
+    );
+  };
+
   config = mkIf cfg.enable {
-    services.fprintd = {
-      enable = true;
-    };
-    security.pam.services = {
-      login.fprintAuth = false;
-      sudo.fprintAuth = true;
-      xfce4-screensaver.fprintAuth = true;
-    };
+    services.fprintd.enable = true;
+    security.pam.services.login.fprintAuth = false;
   };
 }
--- modules/legacy/20-implementations/nixos/features/yubikey.nix
+++ modules/legacy/20-implementations/nixos/features/yubikey.nix
@@ -4,18 +4,20 @@ let
   cfg = config.my.features.yubikey;
 in
 {
+  options.security.pam.services = mkOption {
+    type = types.attrsOf (
+      types.submodule {
+        config.u2fAuth = mkDefault true;
+      }
+    );
+  };
+
   # https://nixos.wiki/wiki/Yubikey
   config = mkIf cfg.enable {
     services.pcscd.enable = true;
     security.pam = {
       u2f.settings.cue = true;
-
-      services = {
-        login.u2fAuth = false;
-        sudo.u2fAuth = true;
-        xfce4-screensaver.u2fAuth = true;
-        polkit-1.u2fAuth = true;
-      };
+      services.login.u2fAuth = false;
     };
   };
 }
