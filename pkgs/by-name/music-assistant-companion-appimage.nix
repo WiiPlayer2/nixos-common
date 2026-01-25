eb9842fd5fbe14ecbc061d0ef47b3b8f3f8e1f54
Waldemar Tomme <waldetomme@gmail.com>
2026-01-25 14:05:42 +0100

    Update music-assistant-companion-appimage

--- pkgs/by-name/music-assistant-companion-appimage.nix
+++ pkgs/by-name/music-assistant-companion-appimage.nix
@@ -5,7 +5,7 @@
 }:
 let
   pname = "music-assistant-companion-appimage";
-  version = "0.0.88";
+  version = "0.1.0";
   icon = fetchurl {
     url = "https://github.com/music-assistant/desktop-companion/blob/d005a1be801e569c1c854fc923e89f58727d901f/app-icon.png?raw=true";
     hash = "sha256-fC0gm54cmla5ut3sqcK58Jp4bpR80eglvj1jtyfvvL4=";
@@ -25,8 +25,8 @@ appimageTools.wrapType2 {
   inherit pname version;
 
   src = fetchurl {
-    url = "https://github.com/music-assistant/tauri-companion-app/releases/download/v${version}/Music.Assistant.Companion_${version}_amd64.AppImage";
-    hash = "sha256-13BCA6kHwvdgxkTEDxLvK1VVWA3Uy8I2AZDzxwqHx60=";
+    url = "https://github.com/music-assistant/desktop-app/releases/download/v${version}/Music.Assistant_${version}_aarch64.AppImage";
+    hash = "sha256-KI0ux7pOSOgN9bZeRMEqlOWPD2wVrjoZDHKgaq3j98w=";
   };
 
   extraInstallCommands = ''
