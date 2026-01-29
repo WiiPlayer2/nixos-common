f0e8e3dcea65c9c1851cb46c432e89d1231821c3
CI <ci@home>
2026-01-29 04:17:29 +0000

    amnesia: 0.1.0 -> 0.2.0
    Diff: https://github.com/cedws/amnesia/compare/v0.1.0...v0.2.0

--- pkgs/by-name/amnesia.nix
+++ pkgs/by-name/amnesia.nix
@@ -4,17 +4,17 @@
 }:
 let
   pname = "amnesia";
-  version = "0.1.0";
+  version = "0.2.0";
   src = fetchFromGitHub {
     owner = "cedws";
     repo = "amnesia";
     rev = "v${version}";
-    hash = "sha256-hcyfEcmmuel2crCDMUdJ5QKVhwgEnDPvS6Ra2f2NTKA=";
+    hash = "sha256-V2IYWXbd3qtYuqBdDYIBvMHnwOj493Q0nQkGYi4jCdA=";
   };
 in
 buildGoModule {
   inherit pname version src;
-  vendorHash = "sha256-vHKDp+zUHEuOB2aYQWHEgTrtEQSzr9kCUZ0FUkQlRTc=";
+  vendorHash = "sha256-QgnkvL+GVRM5vnzgVk+C3PJW4onNt3p0yeaH+pdtfvA=";
 
   meta.mainProgram = "amnesia";
 }
