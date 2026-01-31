918c679821e82ad6e9b7ecefb2da27c1ac98d53c
Waldemar Tomme <waldetomme@gmail.com>
2026-01-31 22:55:04 +0100

    Add gaming aspect with helper script for ludusavi + lutris

---
+++ flake/_tree/aspects/gaming/default.nix
@@ -0,0 +1,26 @@
+{ lib, ... }:
+with lib;
+{
+  flake.aspects.gaming = {
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          (
+            let
+              launcher = writeShellScriptBin "ludusavi-wrap-lutris-run" ''
+                export LD_LIBRARY_PATH="$__ludusavi__LD_LIBRARY_PATH"
+                unset __ludusavi__LD_LIBRARY_PATH
+                exec "$@"
+              '';
+            in
+            writeShellScriptBin "ludusavi-wrap-lutris" ''
+              export __ludusavi__LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+              unset LD_LIBRARY_PATH
+              exec ${getExe ludusavi} wrap --infer lutris --gui --force --no-force-cloud-conflict -- ${getExe launcher} "$@"
+            ''
+          )
+        ];
+      };
+  };
+}
