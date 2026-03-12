33ac2bc33d0075c278caf864fc54bc713ab01136
Waldemar Tomme <waldemar.tomme@bluehands.de>
2026-03-12 12:46:33 +0100

    Add ai-coding aspect

---
+++ nix/aspects/ai-coding/nixos.nix
@@ -0,0 +1,142 @@
+_:
+{ lib, pkgs, ... }:
+with lib;
+let
+  llama-cpp = pkgs.llama-cpp.override {
+    vulkanSupport = true;
+    rocmSupport = false; # explicitly disabled, because unstable
+  };
+  llama-server = lib.getExe' llama-cpp "llama-server";
+
+  ttl = 900; # 15min
+  smallerContextSize = 16 * 1024;
+  mkLlamaModel =
+    {
+      filePath,
+      noCuda ? false,
+      noVulkan ? false,
+      contextSize ? 0,
+    }:
+    {
+      inherit ttl;
+      env = [
+        "HOME=/tmp"
+      ]
+      ++ optional noCuda "CUDA_VISIBLE_DEVICES="
+      ++ optional noVulkan "GGML_VK_VISIBLE_DEVICES=";
+      cmd = ''
+        ${llama-server} \
+          --port ''${PORT} \
+          -m ${escapeShellArg filePath} \
+          --no-warmup \
+          --no-webui \
+          --parallel 1 \
+          --ctx-size ${toString contextSize} \
+          --jinja
+      '';
+    };
+  mkLLamaModels =
+    {
+      name,
+      filePath,
+    }:
+    let
+      variants = {
+        "${name}-min" = mkLlamaModel {
+          inherit filePath;
+          contextSize = smallerContextSize;
+        };
+        "${name}-min-vulkan" = mkLlamaModel {
+          inherit filePath;
+          noCuda = true;
+          contextSize = smallerContextSize;
+        };
+        "${name}-min-cpu" = mkLlamaModel {
+          inherit filePath;
+          noCuda = true;
+          noVulkan = true;
+          contextSize = smallerContextSize;
+        };
+        "${name}" = mkLlamaModel {
+          inherit filePath;
+        };
+        "${name}-vulkan" = mkLlamaModel {
+          inherit filePath;
+          noCuda = true;
+        };
+        "${name}-cpu" = mkLlamaModel {
+          inherit filePath;
+          noCuda = true;
+          noVulkan = true;
+        };
+      };
+    in
+    variants;
+
+  mkModelDownload =
+    {
+      repo,
+      fileName,
+    }:
+    let
+      url = "https://huggingface.co/${repo}/resolve/main/${fileName}?download=true";
+      targetPath = "/var/lib/llama-cpp/models/${fileName}";
+    in
+    ''
+      ${getExe pkgs.wget} -O ${escapeShellArg targetPath} ${escapeShellArg url}
+    '';
+  modelDownloads = [
+    {
+      repo = "unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF";
+      fileName = "Qwen3-Coder-30B-A3B-Instruct-UD-IQ1_S.gguf";
+    }
+  ];
+  modelDownloadScript = pkgs.writeShellApplication {
+    name = "download-llm-models";
+    text = ''
+      ${join "\n" (map mkModelDownload modelDownloads)}
+    '';
+  };
+in
+{
+  environment.systemPackages = [
+    modelDownloadScript
+    llama-cpp
+  ];
+
+  services = {
+    llama-swap = {
+      enable = true;
+      port = 8090;
+      settings = {
+        # healthCheckTimeout = 3600; # 1h
+
+        sendLoadingState = true;
+
+        models =
+          (mkLLamaModels {
+            name = "qwen3-coder-30b-a3b-instruct";
+            filePath = "/var/lib/llama-cpp/models/Qwen3-Coder-30B-A3B-Instruct-UD-IQ1_S.gguf";
+          })
+          // (mkLLamaModels {
+            name = "devstral-small-2-24b-instruct";
+            filePath = "/var/lib/llama-cpp/models/Devstral-Small-2-24B-Instruct-2512-UD-IQ1_S.gguf";
+          })
+          // (mkLLamaModels {
+            name = "ministral3-3b-reasoning";
+            filePath = "/var/lib/llama-cpp/models/Ministral-3-3B-Reasoning-2512-Q4_K_M.gguf";
+          })
+          // (mkLLamaModels {
+            name = "ministral3-3b-instruct";
+            filePath = "/var/lib/llama-cpp/models/Ministral-3-3B-Instruct-2512-Q4_K_M.gguf";
+          });
+      };
+    };
+
+    open-webui = {
+      enable = true;
+      port = 8091;
+      environment.WEBUI_AUTH = "False";
+    };
+  };
+}
