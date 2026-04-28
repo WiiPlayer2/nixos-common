b74aeca38db05a3af452d369ff17defe9a3dbfc7
Waldemar Tomme <waldetomme@gmail.com>
2026-04-28 13:31:42 +0200

    Add icon

--- nix/aspects/live-transcription/nixos/transcription-suite.nix
+++ nix/aspects/live-transcription/nixos/transcription-suite.nix
@@ -1,11 +1,29 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 with lib;
 {
+  # TODO: appimage currently needs a containers/policy.json
   environment.systemPackages = with pkgs; [
     transcription-suite-appimage
   ];
 
-  virtualisation.oci-containers.containers = {
+  warnings =
+    optional (config.virtualisation.podman.enable && config.virtualisation.podman.dockerCompat)
+      ''
+        Podman's docker compatibilty option might not work correctly with Transcription Suite.
+      '';
 
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = mkDefault true;
+    };
+    # oci-containers.containers = {
+
+    # };
   };
 }
--- pkgs/by-name/transcription-suite-appimage.nix
+++ pkgs/by-name/transcription-suite-appimage.nix
@@ -2,35 +2,63 @@
   appimageTools,
   fetchurl,
   makeDesktopItem,
+  writeTextFile,
 }:
 let
   pname = "transcription-appimage";
   version = "1.3.4";
-  # icon = fetchurl {
-  #   url = "https://github.com/music-assistant/desktop-companion/blob/d005a1be801e569c1c854fc923e89f58727d901f/app-icon.png?raw=true";
-  #   hash = "sha256-fC0gm54cmla5ut3sqcK58Jp4bpR80eglvj1jtyfvvL4=";
-  # };
-  # desktopItem = makeDesktopItem {
-  #   name = pname;
-  #   desktopName = "Music Assistant Companion (AppImage)";
-  #   exec = "${pname} %U";
-  #   icon = icon;
-  #   categories = [
-  #     "AudioVideo"
-  #     "Audio"
-  #   ];
-  # };
+  icon = fetchurl {
+    url = "https://github.com/homelab-00/TranscriptionSuite/blob/99ad24d811cba5fee443af07a994745d1cd08d32/docs/assets/logo.png?raw=true";
+    hash = "sha256-1OZ87X3q6s3ibyDq30WgUzvhyTLiqUMcrPuu5a4YRe0=";
+  };
+  desktopItem = makeDesktopItem {
+    name = pname;
+    desktopName = "Transcription Suite (AppImage)";
+    exec = "${pname} %U";
+    icon = icon;
+    categories = [
+      "AudioVideo"
+      "Audio"
+      "Office"
+      "Utility"
+    ];
+  };
+  policy = writeTextFile {
+    name = "transcription-appimage-fhs-container-policy";
+    destination = "/etc/containers/policy.json";
+    text = ''
+      {
+        "default": [
+          {
+            "type": "insecureAcceptAnything"
+          }
+        ],
+        "transports":
+          {
+            "docker-daemon":
+              {
+                "": [{"type":"insecureAcceptAnything"}]
+              }
+          }
+      }
+    '';
+  };
 in
 appimageTools.wrapType2 {
   inherit pname version;
 
   src = fetchurl {
-    url = "https://github.com//homelab-00/TranscriptionSuite/releases/download/v${version}/TranscriptionSuite-${version}.AppImage";
+    url = "https://github.com/homelab-00/TranscriptionSuite/releases/download/v${version}/TranscriptionSuite-${version}.AppImage";
     hash = "sha256-+4f95VR+DnPyjXzkLf/LSz2fD4BQQvrJJICZDK4QM1A=";
   };
 
-  # extraInstallCommands = ''
-  #   mkdir -p "$out/share"
-  #   ln -s "${desktopItem}/share/applications" "$out/share/"
-  # '';
+  extraInstallCommands = ''
+    mkdir -p "$out/share"
+    ln -s "${desktopItem}/share/applications" "$out/share/"
+  '';
+
+  extraPkgs = _: [
+    # TODO: doesn't work T_T
+    policy
+  ];
 }
