5108e16388e798fb3e466ea09cb457361ff0dfc7
Waldemar Tomme <waldetomme@gmail.com>
2026-05-05 12:08:41 +0200

    Remove home.services

--- modules/home-manager/extra/home/services.nix
+++
@@ -1,7 +0,0 @@
-import ../../../_lib/enabled.nix {
-  getNewConfig = x: x.home.services;
-  setNewConfig = x: { home.services = x; };
-  setAttrs = x: { servicesSet = x; };
-  getOldConfig = x: x.services;
-  setOldConfig = x: { services = x; };
-}
