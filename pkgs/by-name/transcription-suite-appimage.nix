d124cc160f26834966648c0bfc89c03d7fa6a5b2
CI <ci@home>
2026-05-06 04:18:26 +0000

    transcription-suite-appimage: 1.3.4 -> 1.3.5
    Diff: https://github.com/homelab-00/TranscriptionSuite/compare/v1.3.4...v1.3.5

--- pkgs/by-name/transcription-suite-appimage.nix
+++ pkgs/by-name/transcription-suite-appimage.nix
@@ -6,7 +6,7 @@
 }:
 let
   pname = "transcription-appimage";
-  version = "1.3.4";
+  version = "1.3.5";
   icon = fetchurl {
     url = "https://github.com/homelab-00/TranscriptionSuite/blob/99ad24d811cba5fee443af07a994745d1cd08d32/docs/assets/logo.png?raw=true";
     hash = "sha256-1OZ87X3q6s3ibyDq30WgUzvhyTLiqUMcrPuu5a4YRe0=";
@@ -49,7 +49,7 @@ appimageTools.wrapType2 {
 
   src = fetchurl {
     url = "https://github.com/homelab-00/TranscriptionSuite/releases/download/v${version}/TranscriptionSuite-${version}.AppImage";
-    hash = "sha256-+4f95VR+DnPyjXzkLf/LSz2fD4BQQvrJJICZDK4QM1A=";
+    hash = "sha256-LAXQjI9GRz1PK3rCoqbJydh0LTXi/TYxGqmHUE7+B7s=";
   };
 
   extraInstallCommands = ''
