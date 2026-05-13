81c62028de4bc4eed8512ee7283e149bd752dfd5
Waldemar Tomme <waldemar.tomme@bluehands.de>
2026-05-13 15:03:37 +0200

    Add deprecated features setting

--- nix/nixosModules/core-impl/module.nix
+++ nix/nixosModules/core-impl/module.nix
@@ -12,10 +12,20 @@
 
   services.openssh.enable = true;
 
-  nix.settings.extra-experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    extra-experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    # specific to Lix
+    extra-deprecated-features = [
+      "broken-string-indentation"
+      "rec-set-dynamic-attrs"
+      "broken-string-escape"
+      "or-as-identifier"
+    ];
+  };
 
   nixpkgs.overlays = [
     inputs.self.overlays.default
