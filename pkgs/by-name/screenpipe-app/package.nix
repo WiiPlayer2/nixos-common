dfb9a0b7f9f0f15368010e775a08083fed1266ef
CI <ci@home>
2026-05-17 04:16:55 +0000

    tockler: 4.0.22 -> 4.0.22

--- pkgs/by-name/screenpipe-app/package.nix
+++ pkgs/by-name/screenpipe-app/package.nix
@@ -18,7 +18,7 @@
 with lib;
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "screenpipe-app";
-  version = "2.4.227";
+  version = "app-v2.4.227";
   src = fetchFromGitHub {
     owner = "screenpipe";
     repo = "screenpipe";
