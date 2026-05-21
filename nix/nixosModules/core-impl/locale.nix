e820420d197a83ebbe6b2e9652e97ba77e80280a
Waldemar Tomme <waldetomme@gmail.com>
2026-05-21 18:07:18 +0200

    Fix module import for systems without workstation module

--- nix/nixosModules/core-impl/locale.nix
+++ nix/nixosModules/core-impl/locale.nix
@@ -1,16 +1,23 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  options,
+  ...
+}:
 with lib;
 {
   time.timeZone = "Europe/Berlin";
   console.keyMap = "de";
 
-  programs.dank-material-shell.greeter.compositor.customConfig =
-    if config.programs.dank-material-shell.greeter.compositor.name == "sway" then
-      ''
-        input "*" {
-          xkb_layout de
-        }
-      ''
-    else
-      warn "No locale config for DankMaterialShell compositor ${config.programs.dank-material-shell.greeter.compositor.name} configured yet." "";
+  programs = optionalAttrs (options.programs ? dank-material-shell) {
+    dank-material-shell.greeter.compositor.customConfig =
+      if config.programs.dank-material-shell.greeter.compositor.name == "sway" then
+        ''
+          input "*" {
+            xkb_layout de
+          }
+        ''
+      else
+        warn "No locale config for DankMaterialShell compositor ${config.programs.dank-material-shell.greeter.compositor.name} configured yet." "";
+  };
 }
