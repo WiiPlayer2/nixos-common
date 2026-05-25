54db13604b2663f91c757a5537d076ab19ad9d9c
CI <ci@home>
2026-05-25 04:18:30 +0000

    screenpipe-app: mcp-v0.18.5 -> 2.4.273
    Diff: https://github.com/screenpipe/screenpipe/compare/app-vmcp-v0.18.5...app-v2.4.273

--- pkgs/by-name/screenpipe-app/package.nix
+++ pkgs/by-name/screenpipe-app/package.nix
@@ -18,15 +18,15 @@
 with lib;
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "screenpipe-app";
-  version = "mcp-v0.18.5";
+  version = "2.4.273";
   src = fetchFromGitHub {
     owner = "screenpipe";
     repo = "screenpipe";
     rev = "app-v${finalAttrs.version}";
-    hash = "sha256-2PNdupqmviUeSpWfFiEIrtZhuBt3adfEUtRhMkvcfLk=";
+    hash = "sha256-cwUeTn5V3hIydehtlTpoHY06tC/ELw3xE65Z5JiTqjo=";
   };
 
-  cargoHash = "sha256-gsEHg5E8GIqduDlS0PMRwvRt2Ax20voy7b/VjLZp4hk=";
+  cargoHash = "sha256-zZdc5ti4ZAzDu/kifTtuDIyQb9EBIIZ+0Dpl2jDoCLc=";
   cargoRoot = "apps/screenpipe-app-tauri/src-tauri";
 
   # npmDeps = fetchNpmDeps {
@@ -86,7 +86,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-gsEHg5E8GIqduDlS0PMRwvRt2Ax20voy7b/VjLZp4hk=";
+    outputHash = "sha256-zZdc5ti4ZAzDu/kifTtuDIyQb9EBIIZ+0Dpl2jDoCLc=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
