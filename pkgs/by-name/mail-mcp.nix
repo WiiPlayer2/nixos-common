81c47f6d08ec0ea08a5212992012fcb0107528c2
CI <ci@home>
2026-05-27 04:16:32 +0000

    mail-mcp: 0.4.7 -> 0.4.8
    Diff: https://github.com/tecnologicachile/mail-mcp/compare/v0.4.7...v0.4.8

--- pkgs/by-name/mail-mcp.nix
+++ pkgs/by-name/mail-mcp.nix
@@ -5,15 +5,15 @@
 }:
 let
   pname = "mail-mcp";
-  version = "0.4.7";
+  version = "0.4.8";
   src = fetchFromGitHub {
     owner = "tecnologicachile";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-0+leg/vF6XawPUQBQMguhNlAAJQIKeCx0XNaV3/ofAI=";
+    hash = "sha256-AeMs2VoOyR78ec0T1q+0g/TdnNOh9RRN6Pyx8km075s=";
   };
 in
 rustPlatform.buildRustPackage {
   inherit pname version src;
-  cargoHash = "sha256-BYlEPxTicb55qI5q0WPObUpR1Hqe2apnrdjcfSqPBYk=";
+  cargoHash = "sha256-RDiIbrFYrHim/daWX/7ajiYjfYdysb3Av7rkIMb0yo8=";
 }
