95ad6f6cc9dadcfd849baaca54d3cb9354cfeb30
CI <ci@home>
2026-06-04 04:19:15 +0000

    screenpipe-app: 2.4.308 -> 2.5.4
    Diff: https://github.com/screenpipe/screenpipe/compare/app-v2.4.308...app-v2.5.4

--- pkgs/by-name/screenpipe-app/package.nix
+++ pkgs/by-name/screenpipe-app/package.nix
@@ -18,12 +18,12 @@
 with lib;
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "screenpipe-app";
-  version = "2.4.308";
+  version = "2.5.4";
   src = fetchFromGitHub {
     owner = "screenpipe";
     repo = "screenpipe";
     rev = "app-v${finalAttrs.version}";
-    hash = "sha256-lDm9Uqe0/E3Yq6IXMwK/cpKH3Y3sfqr4yGxZM3UwnaE=";
+    hash = "sha256-EZLjWw8YfC1xi4s109u8M22Tk6o4XYcxswnKlHVnrZg=";
   };
 
   cargoHash = "sha256-uCeI03EwiPD4KTkEeVeLwnnf8WiyTMc+4IKSJiqYZGY=";
