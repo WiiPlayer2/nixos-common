980379a1c7541370ba646050d5e4e668ea193303
CI <ci@home>
2026-06-07 04:16:15 +0000

    crispasr: 0.6.12 -> 0.7.0
    Diff: https://github.com/CrispStrobe/CrispASR/compare/v0.6.12...v0.7.0

--- pkgs/by-name/crispasr.nix
+++ pkgs/by-name/crispasr.nix
@@ -12,12 +12,12 @@
 with lib;
 let
   pname = "CrispASR";
-  version = "0.6.12";
+  version = "0.7.0";
   src = fetchFromGitHub {
     owner = "CrispStrobe";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-f5CP/A+24IjwhEQ0GPZK4YdGVCGPyuJAmVCnldpVjCs=";
+    hash = "sha256-VP+MCWdlpfrOS2SFqk4gSrurIP4CskYLkdxEFUfJvXE=";
   };
 in
 # whisper-cpp.overrideAttrs {
