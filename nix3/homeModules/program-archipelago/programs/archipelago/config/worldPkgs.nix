e55106838b29b8af611959355eec3869c0ecf892
Waldemar Tomme <waldetomme@gmail.com>
2026-06-07 13:21:24 +0200

    Add & update worlds & packs

--- nix3/homeModules/program-archipelago/programs/archipelago/config/worldPkgs.nix
+++ nix3/homeModules/program-archipelago/programs/archipelago/config/worldPkgs.nix
@@ -1,5 +1,32 @@
 { pkgs }:
 with pkgs;
+let
+  fetchWorld =
+    {
+      name,
+      url,
+      hash,
+      meta ? { inherit name; },
+    }:
+    let
+      src = fetchurl {
+        inherit url hash;
+      };
+      world =
+        runCommand "${name}-apworld"
+          {
+            inherit meta;
+            passthru = {
+              inherit src;
+            };
+          }
+          ''
+            mkdir -p $out
+            ln -sf ${src} "$out/${meta.name}.apworld"
+          '';
+    in
+    world;
+in
 {
   Twilight_Princess =
     let
@@ -64,4 +91,32 @@ with pkgs;
           '';
     in
     world;
+
+  Phantom_Hourglass = fetchWorld {
+    name = "phantom-hourglass";
+    url = "https://github.com/carrotinator/Archipelago/releases/download/ph-v0.9.3-alpha/tloz_ph.apworld";
+    hash = "sha256-/Y2LPR7JBKZNzFc1qBHcHvbVAJVggyp25g21YtQJpNk=";
+    meta.name = "tloz_ph";
+  };
+
+  Spirit_Tracks = fetchWorld {
+    name = "spirit-tracks";
+    url = "https://github.com/DayKat/spirit-tracks/releases/download/st-v0.7.7/tloz_st.apworld";
+    hash = "sha256-7LCeuOcF4GYSY/ZIPoOagOLgwflPaffNWMGwtkyPkD4=";
+    meta.name = "tloz_st";
+  };
+
+  A_Link_Between_Worlds = fetchWorld {
+    name = "a-link-between-worlds";
+    url = "https://github.com/randomsalience/albw-archipelago/releases/download/v0.2.3/albw.apworld";
+    hash = "sha256-p1bJ+9e30sJCiicqIT2XNrao0v/XNSZXB3X8sdprQ9s=";
+    meta.name = "albw";
+  };
+
+  Tracker = fetchWorld {
+    name = "tracker";
+    url = "https://github.com/FarisTheAncient/Archipelago/releases/download/Tracker_v0.2.32/tracker.apworld";
+    hash = "sha256-iLyqOOToCWnhxK0u9QmhH+In9gQ6YgRO+L2Z3jeyKfc=";
+    meta.name = "tracker";
+  };
 }
--- nix3/homeModules/program-poptracker/home/__packs.nix
+++ nix3/homeModules/program-poptracker/home/__packs.nix
@@ -2,8 +2,8 @@
 with pkgs;
 {
   tmcrando_maptracker_deoxis = fetchurl {
-    url = "https://github.com/deoxis9001/tmcrando_maptracker_deoxis/releases/download/stable_v1.0.0.47/StableTmcrTrackerDeoxis.zip";
-    hash = "sha256-Wio2aYYv9ZLRtJa1wFD40A14uOlKXgXz/qMQs7jAoyY=";
+    url = "https://github.com/deoxis9001/tmcrando_maptracker_deoxis/releases/download/stable_v1.0.0.50/StableTmcrTrackerDeoxis.zip";
+    hash = "sha256-XZeCnrk+PCYKSCobpONB39vx9+yYv9ESgp7yGYU0w/k=";
   };
 
   TPRAP_poptracker = fetchurl {
@@ -14,5 +14,31 @@ with pkgs;
   MetroidPrimeAP_PopTrackerPack = fetchurl {
     url = "https://github.com/lilDavid/MetroidPrimeAP-PoptrackerPack/releases/download/1.0.3/MetroidPrime-Archipelago-lilDavid.zip";
     hash = "sha256-PP+r9QeoZSW9qeMHC1GSi9y/aW+9QpoQ53+oSK8QBLA=";
+  };
+
+  PH-AP-Item-Tracker = fetchFromGitHub {
+    name = "PH-AP-Item-Tracker";
+    owner = "ZobeePlays";
+    repo = "PH-AP-Item-Tracker";
+    rev = "b39b3771566a8907a982f4002dabdb1b74b05816";
+    hash = "sha256-hV2cEM4OSwogBveFT8ua9TJNsvxYOthC4CAGJjbqeds=";
+  };
+
+  spirit-tracks-poptracker-ap = fetchurl {
+    url = "https://github.com/carrotinator/spirit-tracks-poptracker-ap/releases/download/v2.0/spirit_tracks_rail_tracker.zip";
+    hash = "sha256-fE2W9JizCzSijczX0vkXpT1KGEDfUZ7tvDFC3l83XyE=";
+  };
+
+  ww-poptracker = fetchFromGitHub {
+    name = "ww-poptracker";
+    owner = "Mysteryem";
+    repo = "ww-poptracker";
+    rev = "v1.3.0";
+    hash = "sha256-7r+kgFpgiOTU47pOcDEec0wnwFimncdR6O3AvWZeb5o=";
+  };
+
+  albw-ap-poptracker = fetchurl {
+    url = "https://github.com/guigui0246/albw-ap-poptracker/releases/download/1.5.2/albw-ap-poptracker-v1.5.2.zip";
+    hash = "sha256-zrxglNSFBhCHVgf6Aqqz6K7UJgn3Ym/d2l0PJi6w5mY=";
   };
 }
