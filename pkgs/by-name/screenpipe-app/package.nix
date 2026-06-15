933fc21772d69a6dabd3e8ebea73e67f6bf15032
CI <ci@home>
2026-06-15 04:19:04 +0000

    screenpipe-app: app-v2.5.37 -> 2.5.37
    Diff: https://github.com/screenpipe/screenpipe/compare/app-vapp-v2.5.37...app-v2.5.37

--- pkgs/by-name/screenpipe-app/package.nix
+++ pkgs/by-name/screenpipe-app/package.nix
@@ -18,7 +18,7 @@
 with lib;
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "screenpipe-app";
-  version = "app-v2.5.37";
+  version = "2.5.37";
   src = fetchFromGitHub {
     owner = "screenpipe";
     repo = "screenpipe";
