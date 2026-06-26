4765157640520b99d207f5c5ff0d73eecf30b1d8
CI <ci@home>
2026-06-26 04:16:25 +0000

    overlayed-appimage: 0.6.2 -> 0.6.2

--- pkgs/by-name/netclaw/package.nix
+++ pkgs/by-name/netclaw/package.nix
@@ -7,12 +7,12 @@
 }:
 let
   pname = "netclaw";
-  version = "0.24.1";
+  version = "0.24.2";
   src = fetchFromGitHub {
     owner = "netclaw-dev";
     repo = "netclaw";
     rev = version;
-    hash = "sha256-xr78WFQcgq3kouxPsPT50XzPF/sAtksOFcqqXSWeQFA=";
+    hash = "sha256-X44NzwlrpATTCI9GE9mBd3tQBPLUpziJo8l+btiU/pQ=";
   };
 in
 buildDotnetModule {
