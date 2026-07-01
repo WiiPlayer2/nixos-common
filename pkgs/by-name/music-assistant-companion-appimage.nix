1b72223a1667dbd35a242d86abdc0bc4dd8e7324
CI <ci@home>
2026-07-01 04:16:38 +0000

    music-assistant-companion-appimage: 0.4.3 -> 0.4.4
    Diff: https://github.com/music-assistant/desktop-app/compare/0.4.3...0.4.4

--- pkgs/by-name/music-assistant-companion-appimage.nix
+++ pkgs/by-name/music-assistant-companion-appimage.nix
@@ -5,7 +5,7 @@
 }:
 let
   pname = "music-assistant-companion-appimage";
-  version = "0.4.3";
+  version = "0.4.4";
   icon = fetchurl {
     url = "https://github.com/music-assistant/desktop-companion/blob/d005a1be801e569c1c854fc923e89f58727d901f/app-icon.png?raw=true";
     hash = "sha256-fC0gm54cmla5ut3sqcK58Jp4bpR80eglvj1jtyfvvL4=";
@@ -26,7 +26,7 @@ appimageTools.wrapType2 {
 
   src = fetchurl {
     url = "https://github.com/music-assistant/desktop-app/releases/download/${version}/Music.Assistant_${version}_amd64.AppImage";
-    hash = "sha256-Nln3llL8QGIjckMp3QhF6+vNyFVqG2DH6PgLYxOhXY8=";
+    hash = "sha256-wRpX4jQAqdBZSTAiAdRtZ68L9xseF2LtTKc1MjWgbic=";
   };
 
   extraInstallCommands = ''
