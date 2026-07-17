f9bdfce571f66ef5fd8eae48ca2620afa8b493fc
Waldemar Tomme <waldetomme@gmail.com>
2026-07-17 15:06:33 +0200

    (Hot)Fix randovania

--- pkgs/by-name/randovania.nix
+++ pkgs/by-name/randovania.nix
@@ -1,15 +1,18 @@
 {
   fetchzip,
   stdenv,
+
   autoPatchelfHook,
   makeDesktopItem,
-  symlinkJoin,
-  writeShellScriptBin,
+  copyDesktopItems,
+  makeWrapper,
+
   libglvnd,
   SDL2,
   libdrm,
   libxcrypt-legacy,
   lttng-ust_2_12,
+  xkeyboard_config,
 }:
 let
   pname = "randovania";
@@ -18,52 +21,49 @@ let
     url = "https://github.com/randovania/randovania/releases/download/v${version}/randovania-${version}-linux.tar.gz";
     hash = "sha256-0QriKkh8SJgNn1PWFcQgEH51wcjUADf72taU22eehCg=";
   };
-  binary-pkgs = stdenv.mkDerivation {
-    inherit pname src version;
-
-    nativeBuildInputs = [
-      autoPatchelfHook
-    ];
-
-    buildInputs = [
-      libglvnd
-      SDL2
-      libdrm
-      libxcrypt-legacy
-      lttng-ust_2_12
-    ];
-
-    autoPatchelfIgnoreMissingDeps = [
-      "libtiff.so.5"
-    ];
-
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/{bin,lib}
-      cp randovania $out/bin/randovania
-      cp -r _internal/* $out/lib/
-      ln -s $out/lib $out/bin/_internal
-      runHook postInstall
-    '';
-  };
-  desktop-item = makeDesktopItem {
-    name = "randovania-desktop";
-    desktopName = "Randovania";
-    categories = [
-      "Game"
-    ];
-    exec = "${binary-pkgs}/bin/randovania";
-  };
-  wrapper = writeShellScriptBin "randovania-wrapper" ''
-    exec ${binary-pkgs}/bin/randovania "$@"
-  '';
-  combined = symlinkJoin {
-    name = binary-pkgs.name;
-    paths = [
-      wrapper
-      desktop-item
-    ];
-    inherit pname version src;
-  };
 in
-combined
+stdenv.mkDerivation {
+  inherit pname src version;
+
+  nativeBuildInputs = [
+    makeWrapper
+    autoPatchelfHook
+    copyDesktopItems
+  ];
+
+  buildInputs = [
+    libglvnd
+    SDL2
+    libdrm
+    libxcrypt-legacy
+    lttng-ust_2_12
+  ];
+
+  autoPatchelfIgnoreMissingDeps = [
+    "libtiff.so.5"
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/{bin,lib}
+    cp randovania $out/bin/randovania
+    cp -r _internal/* $out/lib/
+    ln -s $out/lib $out/bin/_internal
+
+    wrapProgram $out/bin/randovania \
+      --set QT_QPA_PLATFORM xcb
+
+    runHook postInstall
+  '';
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "randovania-desktop";
+      desktopName = "Randovania";
+      categories = [
+        "Game"
+      ];
+      exec = "randovania";
+    })
+  ];
+}
