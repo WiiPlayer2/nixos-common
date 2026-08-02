e94da201963d68615f3806945c5d807078bddead
CI <ci@home>
2026-08-02 04:27:58 +0000

    netclaw: 0.25.1 -> 0.25.2
    Diff: https://github.com/netclaw-dev/netclaw/compare/0.25.1...0.25.2

--- pkgs/by-name/netclaw/package.nix
+++ pkgs/by-name/netclaw/package.nix
@@ -7,12 +7,12 @@
 }:
 let
   pname = "netclaw";
-  version = "0.25.1";
+  version = "0.25.2";
   src = fetchFromGitHub {
     owner = "netclaw-dev";
     repo = "netclaw";
     rev = version;
-    hash = "sha256-60udMIUevWXYq6oT7LLFn2DTK+J0CPfrUA4trblhxF8=";
+    hash = "sha256-4wq3xgsEn7X7/c+9TYG82euulaeOz1FL/7JmGGIFbbY=";
   };
 in
 buildDotnetModule {
