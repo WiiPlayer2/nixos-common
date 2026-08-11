821e29e21af036de0c49017c962a686ed7b23d79
Waldemar Tomme <waldemar.tomme@bluehands.de>
2026-08-11 11:05:55 +0200

    Make teams screenshare preview window floating and sticky

--- nix/aspects/workstation/homeManager/sway/default.nix
+++ nix/aspects/workstation/homeManager/sway/default.nix
@@ -139,6 +139,10 @@ in
         {
           app_id = "me.iepure.devtoolbox";
         }
+        {
+          app_id = "teams-for-linux";
+          title = "Teams for Linux - Screen is being shared";
+        }
       ];
     };
     extraConfig = ''
@@ -162,6 +166,7 @@ in
         [class="Overlayed" title="Overlayed - Main"] sticky enable, blur disable, shadows disable
         [app_id="org.keepassxc.KeePassXC"] sticky enable
         # [app_id="teams-for-linux" floating] sticky enable, resize set 320 240, move position 5 ppt 5 ppt # WIP
+        [app_id="teams-for-linux" title="Teams for Linux - Screen is being shared"] sticky enable, resize set 320 240, move position 5 ppt 5 ppt, border pixel
         [app_id="one.alynx.showmethekey" title="Floating Window - Show Me The Key"] floating enable, sticky enable
 
         [app_id="wdisplays"] resize set 50 ppt 50 ppt
