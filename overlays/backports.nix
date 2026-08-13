7957fd6b99607ca937396f7f0161898dc3ea1ace
Waldemar Tomme <waldetomme@gmail.com>
2026-08-13 13:02:06 +0200

    Remove freecad and bottles backport

--- overlays/backports.nix
+++ overlays/backports.nix
@@ -2,8 +2,6 @@
 with lib;
 let
   backports = [
-    (port "nixpkgs-unstable-small" "freecad" "1.1.1")
-    (port "nixpkgs-unstable-small" "bottles" "64.1")
     (port "nixpkgs-unstable-small" "lact" "0.9.1")
   ];
 
