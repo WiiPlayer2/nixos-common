dcb0043a4bb0f9a93f7a079306b83ae61bee1a47
CI <ci@home>
2026-08-13 03:52:40 +0000

    screenpipe: 2.6.10 -> 2.6.11
    Diff: https://github.com/screenpipe/screenpipe/compare/app-v2.6.10...app-v2.6.11

--- pkgs/by-name/screenpipe/package.nix
+++ pkgs/by-name/screenpipe/package.nix
@@ -20,15 +20,15 @@
 with lib;
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "screenpipe";
-  version = "2.6.10";
+  version = "2.6.11";
   src = fetchFromGitHub {
     owner = "screenpipe";
     repo = "screenpipe";
     rev = "app-v${finalAttrs.version}";
-    hash = "sha256-TWXQyTKl4ikKDMuPeTN/iCE3SxxOCKGJ6V+Eznbyi9Y=";
+    hash = "sha256-cvs15g+zCRz0/wGtu6YcSYAguWfeDhj6dtbe/wh3cGI=";
   };
 
-  cargoHash = "sha256-CtiZKKnUaKZY9N8jdpk74NcqFYk2tz9wjOuymbKNwVw=";
+  cargoHash = "sha256-deyekXU/3rzK4MdD+6v5Uq7VcVfRajNMlHtEJ116bfc=";
 
   nativeBuildInputs = [
     pkg-config
