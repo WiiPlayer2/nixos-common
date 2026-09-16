a8c6cb2890cb79cf93782a653aea195fd661c78f
Waldemar Tomme <waldemar.tomme@bluehands.de>
2026-09-16 12:27:08 +0200

    Allow overlays via nixpkgs.overlaysEx for nix-on-droid configs

--- nix/flakeModules/nixDir/_module/loaders/nix-on-droid.nix
+++ nix/flakeModules/nixDir/_module/loaders/nix-on-droid.nix
@@ -10,10 +10,28 @@ with config.nixDir.lib;
   nixDir.loaders.nixOnDroidConfigurations = presets.modules {
     apply =
       module:
-      inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+      let
+        overlaysModule =
+          { options, ... }:
+          {
+            options.nixpkgs.overlaysEx = options.nixpkgs.overlays;
+          };
+
         pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
-        modules = [ module ];
-      };
+        pkgsEx = import inputs.nixpkgs {
+          system = "aarch64-linux";
+          overlays = config.config.nixpkgs.overlaysEx;
+        };
+        config = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+          inherit pkgs;
+          modules = [
+            overlaysModule
+            module
+          ];
+          extraSpecialArgs.pkgs = pkgsEx;
+        };
+      in
+      config;
 
     config.aliases = [ "nixOnDroid" ];
   };
