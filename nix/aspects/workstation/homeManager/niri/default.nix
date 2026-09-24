d457457648ceeadaeb62d28f0f49a2f4a53c4bee
Waldemar Tomme <waldetomme@gmail.com>
2026-09-24 21:02:45 +0200

    Add some window rules

--- nix/aspects/workstation/homeManager/niri/default.nix
+++ nix/aspects/workstation/homeManager/niri/default.nix
@@ -220,6 +220,33 @@ in
             clip-to-geometry = true;
           };
         }
+        {
+          window-rule = {
+            match._props.is-floating = true;
+            baba-is-float = true;
+          };
+        }
+        {
+          window-rule = {
+            match._props.app-id = ''^org\.wezfurlong\.wezterm$'';
+            default-column-width = { };
+          };
+        }
+        {
+          window-rule = {
+            match._props = {
+              app-id = "firefox$";
+              title = "^Picture-in-Picture$";
+            };
+            open-floating = true;
+          };
+        }
+        {
+          window-rule = {
+            match._props.app-id = ''^org\.keepassxc\.KeePassXC$'';
+            block-out-from = "screen-capture";
+          };
+        }
       ];
     };
   };
